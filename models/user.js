15bb8b643bcb279bce6a84f1a5f8c2da977e33b1
monosniper <monosniper@gmail.com>
2024-02-10 22:08:04 +0500

    dasda

--- models/user.js
+++ models/user.js
@@ -1,58 +1,63 @@
 const db = require('../db')
 const {DataTypes, UUIDV4, DATE, Model} = require('sequelize')
 
-class User extends Model {}
+class User extends Model {
+}
 
 const model = User.init({
     id: {
-	type: DataTypes.INTEGER,
-	primaryKey: true,
-	autoIncrement: true,
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
     },
     key: {
-	type: DataTypes.STRING(255),
-	unique: true,
+        type: DataTypes.STRING(255),
+        unique: true,
     },
     type: {
-	type: DataTypes.ENUM('miner', 'nft'),
+        type: DataTypes.ENUM('miner', 'nft'),
     },
     status: {
-	type: DataTypes.ENUM('start', 'stop'),
-	defaultValue: 'stop'
+        type: DataTypes.ENUM('start', 'stop'),
+        defaultValue: 'stop'
     },
     balance: {
-	type: DataTypes.JSON,
-	defaultValue: "{}"
+        type: DataTypes.JSON,
+        defaultValue: "{}"
     },
     balance_nft: {
-	type: DataTypes.DOUBLE,
-	defaultValue: 0,
+        type: DataTypes.DOUBLE,
+        defaultValue: 0,
     },
     premium: {
-	type: DataTypes.BOOLEAN,
-	defaultValue: false,
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
     },
     isFirstStart: {
-	type: DataTypes.BOOLEAN,
-	defaultValue: true,
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
     },
     isDemo: {
-	type: DataTypes.BOOLEAN,
-	defaultValue: false,
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+    },
+    isNetwork: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
     },
     isDemoExpired: {
-	type: DataTypes.BOOLEAN,
-	defaultValue: false,
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
     },
     from_name: {
-	type: DataTypes.STRING(255),
+        type: DataTypes.STRING(255),
     },
     experience: {
-	type: DataTypes.INTEGER,
-	defaultValue: 0,
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
     },
-    createdAt: { type: DATE, field: 'created_at' },
-    updatedAt: { type: DATE, field: 'updated_at' },
+    createdAt: {type: DATE, field: 'created_at'},
+    updatedAt: {type: DATE, field: 'updated_at'},
 }, {
     sequelize: db,
     tableName: 'users',
